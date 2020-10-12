7a2953c926d23ab123fd3d5d74056895a2a5152c
Anis Halleb <halleb.anis6@gmail.com>
2020-10-12 13:34:50 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Profile from './profile/profile'
+import pic1 from './img/pic.jpg'
+//import pic2 from './img/pic2.jpg'
 
 function App() {
+ const handleName=(x)=>{
+    alert(x);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile fullName="John Wayne" profession="Actor" bio='John Wayne [ d͡ʒɑn weɪn]2, pseudonyme de Marion Mitchell Morrison (né Marion Robert Morrison), est un acteur, réalisateur et producteur américain né le 26 mai 1907 à Winterset (Iowa) et mort le 11 juin 1979 à Los Angeles.' 
+                handleName={handleName} >
+       <img src={pic1} alt="img" style={{width:"150px"}}/>
+      </Profile>
     </div>
   );
+  
 }
 
 export default App;
---
+++ src/profile/profile.js
@@ -0,0 +1,44 @@
+import React, { Children } from 'react';
+import PropTypes from 'prop-types';
+
+
+let Profile=(props)=>{
+return(
+<div>
+    <div>{props.children}</div>
+    <div style={{display:"flex", justifyContent:"space-between", width:"400px"}}>
+       <div>
+           {/* <>{props.handlName(props.fullName)}</> */}
+           <h2>Full Name</h2>
+           <div onLoad={props.handleName(props.fullName)}>{props.fullName}</div>
+           <h2>Age</h2>
+           <div>{props.age}</div>
+           <h2>Is he Dead?</h2>
+           <div>{props.isDead.toString()}</div>
+       </div>
+       <div>
+           <h2>Profession</h2>
+           <div>{props.profession}</div>
+       </div>
+    </div>
+    <div>
+       <h2>BIO</h2>
+       <div style={{width:"300px",border:"solid 2px",padding:"5px"}}>{props.bio}</div>
+    </div>
+</div>
+);
+}
+Profile.defaultProps ={
+  age:75,
+  isDead:true
+};
+Profile.propTypes={
+    fullName:PropTypes.string,
+    profession:PropTypes.string,
+    bio:PropTypes.string,
+    age:PropTypes.number,
+    isDead:PropTypes.bool
+};
+
+
+export default Profile;
